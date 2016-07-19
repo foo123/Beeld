70b1102470bca564a744806afab1d129a3a476e9
Nikos M <nikosms0@gmail.com>
2016-07-19 18:33:08 +0300

    update UMD templates

--- templates/umd-header-plugin.tpl.js
+++ templates/umd-header-plugin.tpl.js
@@ -1,11 +1,11 @@
 !function( root, factory ){
 "use strict";
 if ( ('object'===typeof module) && module.exports ) /* CommonJS */
-    module.exports = factory( (module.$deps && module.$deps["@@MODULE@@"]) || require("./@@MODULE@@".toLowerCase()) );
+    module.exports = factory.call(root,(module.$deps && module.$deps["@@MODULE@@"]) || require("./@@MODULE@@".toLowerCase()));
 else if ( ("function"===typeof define) && define.amd && ("function"===typeof require) && ("function"===typeof require.specified) && require.specified("@@PLUGIN@@") /*&& !require.defined("@@PLUGIN@@")*/ ) 
-    define("@@PLUGIN@@",['module',"@@MODULE@@"],function(mod,module){factory.moduleUri = mod.uri; factory(module); return module;});
+    define("@@PLUGIN@@",['module',"@@MODULE@@"],function(mod,module){factory.moduleUri = mod.uri; factory.call(root,module); return module;});
 else /* Browser/WebWorker/.. */
-    (factory(root["@@MODULE@@"])||1)&&('function'===typeof define)&&define.amd&&define(function(){return root["@@MODULE@@"];} );
+    (factory.call(root,root["@@MODULE@@"])||1)&&('function'===typeof define)&&define.amd&&define(function(){return root["@@MODULE@@"];} );
 }(  /* current root */          @@ROOT@@, 
     /* module factory */        function ModuleFactory__@@PLUGIN@@( @@MODULE@@ ){
 /* main code starts here */
